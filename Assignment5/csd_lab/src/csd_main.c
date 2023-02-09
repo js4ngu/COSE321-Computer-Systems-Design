bbd6b30c2c78e93a28f4d0366ab7652642c07eb9
Jongsang yoo <jamin0513@gmail.com>
2023-02-09 18:57:05 +0900

    assign5 init need to modify

---
+++ Assignment5/csd_lab/src/csd_main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "platform.h"
+#include "xil_printf.h"
+#include <xgpio.h>
+#include "sleep.h"
+#include "xparameters.h"
+#include "sleep.h"
+
+int csd_main() {
+    return 0;
+}
---
+++ Assignment5/csd_platform/platform.tcl
@@ -0,0 +1,28 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct C:\File\Lec\undergraduate\COSE321-Computer-Systems-Design\Assignment1\csd_platform\platform.tcl
+# 
+# OR launch xsct and run below command.
+# source C:\File\Lec\undergraduate\COSE321-Computer-Systems-Design\Assignment1\csd_platform\platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {csd_platform}\
+-hw {C:\File\Lec\undergraduate\COSE321-Computer-Systems-Design\Assignment1\class_experiments_v21\SoC_wrapper.xsa}\
+-proc {ps7_cortexa9_0} -os {standalone} -out {C:/File/Lec/undergraduate/COSE321-Computer-Systems-Design/Assignment1}
+
+platform write
+platform generate -domains 
+platform active {csd_platform}
+platform generate
+platform active {csd_platform}
+domain active {zynq_fsbl}
+bsp reload
+domain active {standalone_domain}
+bsp reload
+platform generate -domains 
+platform generate
+platform generate
+platform active {csd_platform}
+platform generate -domains 
