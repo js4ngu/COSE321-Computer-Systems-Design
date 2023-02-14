9eb6b478494c40938e84e230f23e60c6ffc75a2a
Jongsang yoo <jamin0513@gmail.com>
2023-02-14 15:05:17 +0900

    assign 9 init

---
+++ Assignment9/csd_platform/platform.tcl
@@ -0,0 +1,23 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct C:\File\Lec\undergraduate\COSE321-Computer-Systems-Design\Assignment1\csd_platform\platform.tcl
+# 
+# OR launch xsct and run below command.
+# source C:\File\Lec\undergraduate\COSE321-Computer-Systems-Design\Assignment1\csd_platform\platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {csd_platform}\
+-hw {C:\File\Lec\undergraduate\COSE321-Computer-Systems-Design\Assignment1\class_experiments_v21\SoC_wrapper.xsa}\
+-proc {ps7_cortexa9_0} -os {standalone} -out {C:/File/Lec/undergraduate/COSE321-Computer-Systems-Design/Assignment1}
+
+platform write
+platform generate -domains 
+platform active {csd_platform}
+platform generate
+platform generate -domains zynq_fsbl 
+platform active {csd_platform}
+platform generate -domains 
+platform active {csd_platform}
+platform generate -domains 
